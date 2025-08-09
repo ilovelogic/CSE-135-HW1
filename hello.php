86813859890d8a0b1535aae085cd896936a97bc0
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-09 00:31:37 +0000

    Finished the metaFilled in missing meta arguments

--- hello.php
+++ hello.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <head>
         <meta charset="UTF-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale>        <title>Hello PHP</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />        
+        <title>Hello PHP</title>
 </head>
 <body>
         <h1>PHP Lives!</h1>
