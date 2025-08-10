88d2a515f45dd619838f3c7a224b050e6c77f151
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-10 23:04:58 +0000

    Changeed color ordering

--- members/script.js
+++ members/script.js
@@ -1,5 +1,5 @@
 const factArray = ["Both my parents earned their Ph.D.s at UCSD.", "I know a few Latin songs by heart.", "My favorite proof is the one for the infinitude of primes.", "I have eight siblings.", "I composed a love poem about mathematics at the age of fourteen.", "I was homeschooled."];
-const colArray = ["red", "blue", "pink", "green","orange"];
+const colArray = ["blue", "red", "pink", "green","orange"];
 let factIndex = 0
 let colIndex = 0;
 
