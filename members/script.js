d6e5204bf542508180dea098622fd5297ea6094f
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-10 23:42:17 +0000

    Changed spacing

--- members/script.js
+++ members/script.js
@@ -1,5 +1,6 @@
 const factArray = ["Both my parents earned their Ph.D.s at UCSD.", "I know a few Latin songs by heart.", "My favorite proof is the one for the infinitude of primes.", "I have eight siblings.", "I composed a love poem about mathematics at the age of fourteen.", "I was homeschooled."];
 const colArray = ["blue", "red", "pink", "green","orange"];
+
 let factIndex = 0
 let colIndex = 0;
 
@@ -7,7 +8,7 @@ function newFact() {
     const div = document.getElementById("randFact");
     div.innerHTML = factArray[factIndex];
     factIndex = (factIndex + 1) % 6;
-    
+
     div.style.color = colArray[colIndex];
     colIndex = (colIndex + 1) % 5;
 }
