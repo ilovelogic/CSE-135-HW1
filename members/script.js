ea6a5618c0b8d8ab8dd97136d996ab61d5585548
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-10 22:28:42 +0000

    Rearranged array contents

--- members/script.js
+++ members/script.js
@@ -1,4 +1,4 @@
-const factArray = ["My favorite proof is the one for the infinitude of primes.", "I have eight siblings.", "Both my parents earned their Ph.D.s at UCSD.", "I know a few Latin songs by heart.", "I was homeschooled.", "I composed a love poem about mathematics at the age of fourteen."];
+const factArray = ["Both my parents earned their Ph.D.s at UCSD.", "I know a few Latin songs by heart.", "My favorite proof is the one for the infinitude of primes.", "I have eight siblings.", "I composed a love poem about mathematics at the age of fourteen.", "I was homeschooled."];
 let cur_index = 0
 
 function newFact() {
