201ec0056df9b7a67a5aa06dc5126f0869535f4f
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-11 19:45:04 +0000

    Making colIndex a global var for debugging

--- members/script.js
+++ members/script.js
@@ -1,15 +1,14 @@
 const factArray = ["Both my parents earned their Ph.D.s at UCSD.", "I know a few Latin songs by heart.", "My favorite proof is the one for the infinitude of primes.", "I have eight siblings.", "I composed a love poem about mathematics at the age of fourteen.", "I was homeschooled."];
 const colArray = ["red", "green", "blue", "yellow","orange"]
 let factIndex = 0;
+let colIndex = 0;
 
 function newFact() {
     const div = document.getElementById("randFact");
 
     div.innerHTML = factArray[factIndex];
+    div.style.color = colArray[colIndex];
     factIndex = (factIndex + 1) % 6;
-
-    let colIndex = (factIndex * 6) % 5;
-    // div.style.color = colArray[colIndex];
-    div.style.color = "red";
+    colIndex = (factIndex * 6) % 5;
 
 }
