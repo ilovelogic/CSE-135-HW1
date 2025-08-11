fb06cf9a735a98daa6f3fe6ee1c779e892b0666d
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-11 19:39:30 +0000

    Corrected lack of "" for the colors

--- members/script.js
+++ members/script.js
@@ -1,31 +1,14 @@
 const factArray = ["Both my parents earned their Ph.D.s at UCSD.", "I know a few Latin songs by heart.", "My favorite proof is the one for the infinitude of primes.", "I have eight siblings.", "I composed a love poem about mathematics at the age of fourteen.", "I was homeschooled."];
-
+const colArray = ["pink", "green", "blue", "purple","orange"]
 let factIndex = 0;
 
 function newFact() {
     const div = document.getElementById("randFact");
-    div.style.color = pink;
+
     div.innerHTML = factArray[factIndex];
     factIndex = (factIndex + 1) % 6;
 
-    let colVal = (factIndex * 6) % 5;
-
-    switch(colVal) {
-        case 0:
-            div.style.color = pink;
-            break;
-        case 1:
-            div.style.color = blue;
-            break;
-        case 2:
-            div.style.color = purple;
-            break;
-        case 3:
-            div.style.color = orange;
-            break;
-        case 4:
-            div.style.color = green;
-            break;
-    }
+    let colIndex = (factIndex * 6) % 5;
+    div.style.color = colArray[colIndex];
 
 }
