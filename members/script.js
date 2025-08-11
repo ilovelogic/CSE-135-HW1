50fd6b72089ee6fbfc06302fd630ecbf0ef61ef4
Anne Kelley <150639582+ilovelogic@users.noreply.github.com>
2025-08-11 19:43:35 +0000

    color bug again :')

--- members/script.js
+++ members/script.js
@@ -1,5 +1,5 @@
 const factArray = ["Both my parents earned their Ph.D.s at UCSD.", "I know a few Latin songs by heart.", "My favorite proof is the one for the infinitude of primes.", "I have eight siblings.", "I composed a love poem about mathematics at the age of fourteen.", "I was homeschooled."];
-const colArray = ["pink", "green", "blue", "purple","orange"]
+const colArray = ["red", "green", "blue", "yellow","orange"]
 let factIndex = 0;
 
 function newFact() {
@@ -9,7 +9,7 @@ function newFact() {
     factIndex = (factIndex + 1) % 6;
 
     let colIndex = (factIndex * 6) % 5;
-    div.style.color = colArray[colIndex];
-    div.style.color = "pink";
+    // div.style.color = colArray[colIndex];
+    div.style.color = "red";
 
 }
